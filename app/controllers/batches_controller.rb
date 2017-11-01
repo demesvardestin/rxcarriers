af44dd1a2b06d3c32fc1a10d0fff460cf0e64d6d
demo07 <dddemesvar07@gmail.com>
2017-11-01 04:06:59 +0000

    commit

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -48,19 +48,17 @@ class BatchesController < ApplicationController
       number = url[num_start..num_start + 11]
       request_response = url[body_start..body_start + 4].downcase
     end
-    if number && request_response
-      initial_request_message = RequestMessage.find_by(driver_number: number)
-      pharmacy = Pharmacy.find_by(id: initial_request_message.pharmacy_id)
-      initial_request = Request.find_by(body: initial_request_message.message_body)
-      if request_response == 'yes'
-        initial_request.update!(status: 'accepted', count: count + 1)
-        Batch.respond_to_drivers(number, pharmacy, initial_request, initial_request.batch_id)
-      elsif request_response == 'can'
-        driver = Driver.find_by(number: number)
-        initial_request = Request.find_by(driver: driver, status: 'accepted', body: initial_request_message)
-        initial_request.update!(status: 'pending', count: 0)
-        Request.resend_request(initial_request.batch_id, initial_request.pharmacy, initial_request, driver)
-      end
+    initial_request_message = RequestMessage.find_by(driver_number: number)
+    pharmacy = Pharmacy.find_by(id: initial_request_message.pharmacy_id)
+    initial_request = Request.find_by(body: initial_request_message.message_body)
+    if request_response == 'yes'
+      initial_request.update!(status: 'accepted', count: count + 1)
+      Batch.respond_to_drivers(number, pharmacy, initial_request, initial_request.batch_id)
+    elsif request_response == 'can'
+      driver = Driver.find_by(number: number)
+      initial_request = Request.find_by(driver: driver, status: 'accepted', body: initial_request_message)
+      initial_request.update!(status: 'pending', count: 0)
+      Request.resend_request(initial_request.batch_id, initial_request.pharmacy, initial_request, driver)
     end
   end
 
