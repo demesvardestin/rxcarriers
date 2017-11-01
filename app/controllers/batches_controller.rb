77e974bb5f4d874848ca90faf400a49dbf99862d
demo07 <dddemesvar07@gmail.com>
2017-11-01 15:19:46 +0000

    commit

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -51,39 +51,40 @@ class BatchesController < ApplicationController
     # params_hash = CGI::parse(URI.parse(url).query)
     # directions = "Thank you for accepting this request. Your pickup is now ready at MedCab.\n
     #                     For verification purposes, present your ID once you arrive.\nTo cancel this pickup, reply 'cancel'."
-    number = params['From']
-    request_response = params['Body']
-    # boot_twilio
-    # @client.api.account.messages.create(
-    #                 from: '+13474640621',
-    #                 to: number,
-    #                 body: directions
-    #             )
-    if number && request_response
-      # fetch the original message sent to drivers. driver number is used since we delete every message we sent to drivers to avoid clogging
-      initial_request_message = RequestMessage.find_by(driver_number: number)
-      # pharmacy is looked up
-      pharmacy = Pharmacy.find_by(id: initial_request_message.pharmacy_id)
-      # figure out the details of the initial request
-      initial_request = Request.find_by(body: initial_request_message.message_body)
-      # decide what to do depending on the driver's response
-      if request_response == 'yes'
-        # if the response is 'yes', update the initial request
-        initial_request.update!(status: 'accepted', count: count + 1)
-        # send directions to driver, notify other drivers
-        Batch.respond_to_drivers(number, pharmacy, initial_request, initial_request.batch_id)
-      elsif request_response == 'can'
-        # this response means a driver previously accepted a request, and is now cancelling
-        # so we first fetch the driver
-        driver = Driver.find_by(number: number)
-        # then the initial request
-        initial_request = Request.find_by(driver: driver, status: 'accepted', body: initial_request_message)
-        # update request to show pending status, and reset count to 0
-        initial_request.update!(status: 'pending', count: 0)
-        # resend the request to all but the cancelling driver
-        Request.resend_request(initial_request.batch_id, initial_request.pharmacy, initial_request, driver)
-      end
-    end
+    # number = params['From']
+    # request_response = params['Body']
+    initialize_twilio
+    # if request_response
+    @client.api.account.messages.create(
+                    from: '+13474640621',
+                    to: number,
+                    body: directions
+                )
+    # if number && request_response
+    #   # fetch the original message sent to drivers. driver number is used since we delete every message we sent to drivers to avoid clogging
+    #   initial_request_message = RequestMessage.find_by(driver_number: number)
+    #   # pharmacy is looked up
+    #   pharmacy = Pharmacy.find_by(id: initial_request_message.pharmacy_id)
+    #   # figure out the details of the initial request
+    #   initial_request = Request.find_by(body: initial_request_message.message_body)
+    #   # decide what to do depending on the driver's response
+    #   if request_response == 'yes'
+    #     # if the response is 'yes', update the initial request
+    #     initial_request.update!(status: 'accepted', count: count + 1)
+    #     # send directions to driver, notify other drivers
+    #     Batch.respond_to_drivers(number, pharmacy, initial_request, initial_request.batch_id)
+    #   elsif request_response == 'can'
+    #     # this response means a driver previously accepted a request, and is now cancelling
+    #     # so we first fetch the driver
+    #     driver = Driver.find_by(number: number)
+    #     # then the initial request
+    #     initial_request = Request.find_by(driver: driver, status: 'accepted', body: initial_request_message)
+    #     # update request to show pending status, and reset count to 0
+    #     initial_request.update!(status: 'pending', count: 0)
+    #     # resend the request to all but the cancelling driver
+    #     Request.resend_request(initial_request.batch_id, initial_request.pharmacy, initial_request, driver)
+    #   end
+    # end
   end
 
   def destroy
@@ -91,7 +92,7 @@ class BatchesController < ApplicationController
   
   private
   
-    def boot_twilio
+    def initialize_twilio
       account_sid = 'AC7b0eae323dc72522bb616648567a7de6'
       auth_token = '2a27c125b10a4429e8a24ccd08584670'
       @client = Twilio::REST::Client.new account_sid, auth_token
