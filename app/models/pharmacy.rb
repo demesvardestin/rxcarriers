843d35a421c877b0ea75c4fd83e3c59f74664085
demo07 <dddemesvar07@gmail.com>
2017-11-02 00:45:18 +0000

    commit

--- app/models/pharmacy.rb
+++ app/models/pharmacy.rb
@@ -4,6 +4,6 @@ class Pharmacy < ActiveRecord::Base
     has_many :patients, :as => :patable
     
     def full_address
-        [town, state, zipcode].join(', ')
+        [street, town, zipcode].join(', ')
     end
 end
