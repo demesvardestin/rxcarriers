6388e1be2aff8008937895dcc6b01de7483c738c
demo07 <dddemesvar07@gmail.com>
2017-11-05 14:43:46 +0000

    commit

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -13,7 +13,7 @@ class Driver < ActiveRecord::Base
         req_type = {true => 'new request', false => 'request resend'}
         # filter all drivers by location before going through selection process
         drivers = Driver.omit_driver(initial_driver)
-        @drivers = Driver.filter_by_location(@pharmacy.full_address, drivers)
+        @drivers = Driver.filter_by_location(pharmacy.full_address, drivers)
         @drivers.available.each do |driver|
             # send a message request to drivers
             self.initialize_twilio.api.account.messages.create(
