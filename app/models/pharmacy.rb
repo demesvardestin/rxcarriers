e1cfd4fb73f90bbd90cf65d3e883974df37e3fb0
demo07 <dddemesvar07@gmail.com>
2017-12-31 04:42:54 +0000

    modified pharmacy model
    removed additional non-sequel search method. was too lengthy,
    bulky, slow, memory and time consuming. sql 'where' method
    works much better.
    
    see pharmacy.rb

--- app/models/pharmacy.rb
+++ app/models/pharmacy.rb
@@ -40,39 +40,4 @@ class Pharmacy < ActiveRecord::Base
       self.card_number.present? && self.exp_year.present? && self.exp_month.present? && self.bill_street.present? && self.bill_city.present? && self.bill_state.present? && self.bill_zip.present?
     end
     
-    def self.search(string)
-      split_string = string.scan(/\w+/)
-      first_start = split_string[0][0..3]
-      first_end = split_string[0][-1,3]
-      last_start = split_string[1][0..3] if split_string.length > 1
-      last_end = split_string[1][-1,3] if split_string.length > 1
-      results = Pharmacy.select do |ph|
-        total = self.check_start(ph.name.downcase.scan(/\w+/)[0], first_start, last_start) + 
-              self.check_end(ph.name.downcase.scan(/\w+/)[1], first_end, last_end) + 
-              self.check_word(ph.name, string)
-        total >= 1
-      end
-      return results
-    end
-    
-    def self.check_start(x, y, z=nil)
-      counter = 0
-      counter += 1 if x.start_with?(y)
-      counter += 1 if z != nil && x.start_with?(z)
-      return counter
-    end
-    
-    def self.check_end(x, y, z=nil)
-      counter = 0
-      counter += 1 if x.end_with?(y)
-      counter += 1 if z != nil && x.end_with?(z)
-      return counter
-    end
-    
-    def self.check_word(x, y)
-      counter = 0
-      x == y ? counter += 1 : counter = 0
-      return counter
-    end
-    
 end
