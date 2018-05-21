39eb3e1b55ad301700ad5ac9958fafd94380e420
demo07 <dddemesvar07@gmail.com>
2018-05-21 12:58:16 +0000

    commit

--- db/migrate/20180331220404_add_signature_image_to_deliveries.rb
+++ db/migrate/20180331220404_add_signature_image_to_deliveries.rb
@@ -2,6 +2,6 @@ class AddSignatureImageToDeliveries < ActiveRecord::Migration
   def change
     add_column :deliveries, :signature_image, :text
     add_column :deliveries, :request_sent_on, :datetime
-    add_column :batches, :deliveries_completed, :integer
+    # add_column :batches, :deliveries_completed, :integer
   end
 end
--- db/migrate/20180420190445_add_delivery_count_to_pharmacies.rb
+++ db/migrate/20180420190445_add_delivery_count_to_pharmacies.rb
@@ -2,6 +2,6 @@ class AddDeliveryCountToPharmacies < ActiveRecord::Migration
   def change
     add_column :pharmacies, :deliveries_today, :integer
     add_column :deliveries, :deleted, :boolean
-    add_column :batches, :deleted, :boolean
+    # add_column :batches, :deleted, :boolean
   end
 end
