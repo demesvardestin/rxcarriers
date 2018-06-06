f5afc09a90b71a837e2058632c81f0f300eecb42
demo07 <dddemesvar07@gmail.com>
2018-06-06 15:41:49 +0000

    commit

--- db/migrate/20180603172908_add_signed_on_to_terms_and_agreements.rb
+++ db/migrate/20180603172908_add_signed_on_to_terms_and_agreements.rb
@@ -1,5 +1,5 @@
 class AddSignedOnToTermsAndAgreements < ActiveRecord::Migration
   def change
-    add_column :terms_and_agreements, :signed_on, :date_time
+    add_column :terms_and_agreements, :signed_on, :datetime
   end
 end
